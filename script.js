1d2fd3b6feddc6e54d4de078db2b878d5f941431
LevShyZzz <jorgeduardomg01@gmail.com>
2025-06-07 01:56:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// filepath: sorpresa-novia/sorpresa-novia/src/script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const miBoton = document.getElementById('miBoton');
+    miBoton.addEventListener('click', () => {
+        alert('¡Sorpresa! Espero que te guste esta página.');
+    });
+});
